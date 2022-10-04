04c67fbf7ecbb8f0d7c34f8541e828fd5842d4d1
leonbos1 <ktmleon@live.nl>
2022-10-04 11:37:52 +0200

    Initial commit

---
+++ CarApp/Controllers/CarsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarApp.Models;
+
+namespace CarApp.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            CarsViewModel Car = new CarsViewModel()
+            {
+                Brand = "Porsche",
+                Model = "911"
+            };
+            
+            return View(Car);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ CarApp/Models/CarsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarApp.Models
+{
+    public class CarsViewModel
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
