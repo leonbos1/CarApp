7bfd7beefdeb9c88c8927387668e8cb93b5279d4
leonbos1 <ktmleon@live.nl>
2022-10-06 22:03:35 +0200

    users start

---
+++ CarApp/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using CarApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<UserModel> users = new List<UserModel>();
+        
+        public IActionResult Index()
+        {
+            
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            var userModel = new UserModel();
+            return View(userModel);
+        }
+
+        public IActionResult CreateUser(UserModel userModel)
+        {
+            Random random = new Random();
+            userModel.Id = random.Next(100000000);
+            users.Add(userModel);
+            return RedirectToAction(nameof(Create));
+
+        }
+    }
+}
---
+++ CarApp/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarApp.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int age { get; set; }
+    }
+}
